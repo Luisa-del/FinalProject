296c561a7ef24e22d00e97cdad623d5d371cd1b5
Luisa <luisa-p@gmx.de>
2020-01-28 20:33:28 +0100

    upload

---
+++ install.packages.R
@@ -0,0 +1,22 @@
+#doesn't install packages
+
+install.packages("devtools")
+library(devtools)
+
+install.packages("RStoolbox")
+library(RStoolbox)
+
+install.packages("tidyverse")
+library(tidyverse)
+
+install.packages("oceanmap")
+library(oceanmap)
+
+install.packages("ncdf4")
+library(ncdf4)
+
+install.packages("ncdf")
+
+
+#QGIS, OTB, Terminal?!  -> vpn
+
